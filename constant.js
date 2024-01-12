fd71d3e7fdd9276aa4373f19c20e96e5a775bbe0
Justin Liu <justinliu@ark7.com>
2024-01-12 09:46:53 -0800

    update ticker

--- constant.js
+++ constant.js
@@ -1,7 +1,7 @@
 //export const tickers = ['U', 'AAPL'];
 
-export const tickers = ['U', 'AAPL', 'MSFT', 'AMZN', 'NVDA', 'GOOGL', 'META', 'TSLA', 'GOOG', 'BRK.B', 'AVGO', 'UNH', 'JPM', 
-  'FB', 'PYPL', 'V', 'HD', 'BABA', 'INTC', 'CMCSA', 'ADBE', 'PFE', 'NFLX', 'TSM', 'CRM', 'KO', 'NKE', 'ABNB', 'XOM', 'PEP',];
-  
+export const tickers = ['U', 'AAPL', 'MSFT', 'AMZN', 'NVDA', 'GOOGL', 'META', 'TSLA', 'GOOG', 
+'BRK.B', 'AVGO', 'UNH', 'JPM', 'PYPL', 'V', 'HD', 'BABA', 'INTC', 'CMCSA', 'ADBE', 'PFE', 
+'NFLX', 'TSM', 'CRM', 'KO', 'NKE', 'ABNB', 'XOM', 'PEP', 'LDOS', 'CPRT', 'GILD', 'LRCX', 'BLDR'];
 export const timePeriod= 5; // track previous ? days information (short + long term prediction)
 
